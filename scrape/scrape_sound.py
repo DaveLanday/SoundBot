2c4a4c14ffdba5d1e1ae23951b52a61e5ec011cd
DaveLanday <14958778+DaveLanday@users.noreply.github.com>
2020-04-09 21:27:29 -0400

    added scrape_sound.py. These are wrappers for the main functions of search_sounds.py. The functions in this file retrieve the links to all the sound boards from a hypothetical keyword search query and then retrieve the playable sounds from that board.

--- scrape/scrape_sound.py
+++ scrape/scrape_sound.py
@@ -3,14 +3,45 @@
  
 #scrape_sound.py
 #Dave Landay
-#LAST UPDATED: 03-26-2020
+#LAST UPDATED: 04-09-2020
 
-from bs4 import BeautifulSoup
-import requests
+import search_sounds
 
-url = 'https://www.soundboard.com/'
-page = requests.get(url)
-print('page stats: ', page.status_code)
-scraper = BeautifulSoup(page.text, 'html.parser')
-print(scraper.prettify())
+def find_boards(keyword):
+    """
+        FIND_BOARDS: Returns a list of sound board links to pass to JS.
+
+        param: keyword (str): A string representing the keyword
+                              or search criteria.
+
+        returns: board_info (dict): A dictionary of soundboards retrieved from a search and their
+                                    meta-information. This includes board title and the boards url.
+    """
+
+    board_info = get_board_from_search(keyword)
+
+    return board_info
+
+def on_board_selection():
+    """
+        MAIN: Runs the webscraper
+    """
+    # Url to specific soundboard (global because it will be updated by user/JS):
+    global board_url
+
+    # Javascript detects click event and changes the state of Global var:
+        
+    # Make the parser:
+    parser = make_parser(board_url)
+    
+    # Check metadata to see if the sound is for personal use:
+    meta_data = get_meta_data(parser)
+
+    if not meta_data:
+        return 'Not available for personal use'
+    else:
+        # Get the track list:
+        tracks = get_tracks(parser)
+    
+        return tracks
 
