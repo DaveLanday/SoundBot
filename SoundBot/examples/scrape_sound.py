cbbe96dbee5de25ec0d39e12230dade9b68a8aa3
DaveLanday <14958778+DaveLanday@users.noreply.github.com>
2020-04-13 23:20:39 -0400

    made a general project folder called SoundBot. Added node modules discord.js and express for serving the bot.Updated .gitignore to ignore node_modules

---
+++ SoundBot/examples/scrape_sound.py
@@ -0,0 +1,52 @@
+#/usr/bin/env python
+# -*-coding: utf-8 -*-
+ 
+#scrape_sound.py
+#Dave Landay
+#LAST UPDATED: 04-09-2020
+import sys
+sys.path.insert(1, '../packages')
+import search_sounds
+
+def find_boards(keyword):
+    """
+        FIND_BOARDS: Returns a list of sound board links to pass to JS.
+
+        param: keyword (str): A string representing the keyword
+                              or search criteria.
+
+        returns: board_info (dict): A dictionary of soundboards retrieved from a search and their
+                                    meta-information. This includes board title and the boards url.
+    """
+
+    board_info = search_sounds.get_board_from_search(keyword)
+
+    return board_info
+
+def on_board_selection(board_url):
+    """
+        ON_BOARD_SELECTION: Runs the webscraper to get soundboard tracks
+
+        param: board_url (str): A string representing the url to a specified
+                                soundboard (the one that the user clicks on 
+                                in the app).
+        
+        returns: tracks (dict): Dictionary containing tracklist and track meta-data.
+                    ... or
+                 message(str): Returns 'Not available for personal use' if board is not for personal use.
+    """
+            
+    # Make the parser:
+    parser = search_sounds.make_parser(board_url)
+    
+    # Check metadata to see if the sound is for personal use:
+    meta_data = search_sounds.get_meta_data(parser)
+
+    if not meta_data:
+        return 'Not available for personal use'
+    else:
+        # Get the track list:
+        tracks = search_sounds.get_tracks(parser)
+    
+        return tracks
+
---
+++ SoundBot/packages/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+ 
+#__init__.py
+#Dave Landay
+#LAST UPDATED: 03-26-2020
+
