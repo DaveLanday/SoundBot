13a1cc23d994dfb1386d0d508256e5a5b25cc2d6
DaveLanday <14958778+DaveLanday@users.noreply.github.com>
2020-04-14 01:53:27 -0400

    Got bot to connect with a discord server

--- SoundBot/index.js
+++ SoundBot/index.js
@@ -1,14 +1,20 @@
 // Load discord library:
 const disc = require("discord.js");
+const { config } = require("dotenv");
 
 // Create a new bot/client:
 const client = new disc.Client();
 
-// Load token and prefix values from config.json:
-const config = require("./config.json");
+// Load token and prefix values from .env:
+config({
+	path: __dirname + "/.env"
+});
 
 // What happens when the bot starts?:
-client.on("ready", () => {
-	console.log('Bot launched. It is being used by' ${client.users.size} 'in' ${client.channel.size} 'channels, in' ${client.guilds.size} ' guilds.');
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.on("ready", () => { 
+	console.log(`Bot launched. Hello, my name is ${client.user.username}. I pass butter and play soundboards.`);
+	// client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
+
+// Login using bot creds:
+client.login(process.env.API_KEY);
